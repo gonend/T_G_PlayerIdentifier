1211def665e56a84199c916acc1d3590dc896c9d
teventov5 <teventov@gmail.com>
2023-01-12 01:24:22 +0200

    added google login and logout functionality

--- frontend/Components/Login/Login.tsx
+++ frontend/Components/Login/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     SafeAreaView,
     StatusBar,
@@ -10,7 +10,9 @@ import {
     Image
 } from 'react-native';
 import auth from '@react-native-firebase/auth';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { GoogleSignin, User } from '@react-native-google-signin/google-signin';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 GoogleSignin.configure({
     webClientId:
@@ -18,52 +20,138 @@ GoogleSignin.configure({
     offlineAccess: true
 });
 
-async function signinWithGoogle() {
-    try {
-        //get the users id Token
-        const { idToken } = await GoogleSignin.signIn();
-        //create a oogle credentials with the token
-        const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-        //sign in the user with the credentials
-        // console.log(`token is: ${idToken}`);
-        // console.log(`google credentials is:`);
-        // console.log(googleCredential);
-        let credentials = auth().signInWithCredential(googleCredential);
-        console.log('====================================');
-        console.log((await credentials).user);
-        console.log('====================================');
-        // console.log(credentials);
-
-        return credentials;
-    } catch (error) {
-        console.log('====================================');
-        console.log('Error in login is: ' + error);
-        console.log('====================================');
-    }
-}
 const Login = () => {
+    const [authorizedUser, setAuthorizedUser] = useState<boolean>(false);
+    const [user, setUser] = useState<User | null>();
+    // const [credentials, setCredentials] =
+    //     useState<FirebaseAuthTypes.UserCredential>();
+
+    async function getUserCredentialsWithToken(idToken: string) {
+        try {
+            const googleCredential =
+                auth.GoogleAuthProvider.credential(idToken);
+            let credentials = await auth().signInWithCredential(
+                googleCredential
+            );
+            return credentials;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async function signInWithGoogle() {
+        try {
+            //get the users id Token
+            const { idToken } = await GoogleSignin.signIn();
+            //create a google credentials with the token
+            let credentials;
+            if (idToken) {
+                credentials = await getUserCredentialsWithToken(idToken);
+                if (credentials?.user) {
+                    let tempUser = await GoogleSignin.getCurrentUser();
+                    console.log(tempUser);
+
+                    setUser(tempUser);
+
+                    // set access token in AsyncStorage storage
+                    AsyncStorage.setItem(
+                        'accessToken',
+                        tempUser?.idToken as string
+                    );
+                    setAuthorizedUser(true);
+                }
+            }
+
+            return credentials;
+        } catch (error) {
+            console.log('====================================');
+            console.log('Error in login is: ' + error);
+            console.log('====================================');
+        }
+    }
+
+    async function logoutUser() {
+        GoogleSignin.signOut();
+        console.log('before:');
+
+        console.log(await AsyncStorage.getItem('accessToken'));
+        AsyncStorage.clear();
+        console.log('after:');
+        console.log(await AsyncStorage.getItem('accessToken'));
+        setUser(null);
+
+        console.log('logged out');
+        setAuthorizedUser(false);
+    }
+
+    useEffect(() => {
+        (async () => {
+            let storageIdToken = await AsyncStorage.getItem('accessToken');
+
+            if (storageIdToken) {
+                let tempUser = await GoogleSignin.getCurrentUser();
+                console.log('====================================');
+                console.log(tempUser);
+                if (tempUser) {
+                    setAuthorizedUser(true);
+                }
+                console.log('====================================');
+            }
+        })();
+    }, []);
+    useEffect(() => {
+        (async () => {
+            if (authorizedUser) {
+                let myUser = await GoogleSignin.getCurrentUser();
+                setUser(myUser);
+            }
+        })();
+    }, [authorizedUser]);
+
     return (
         <SafeAreaView style={styles.safeArea}>
             <StatusBar barStyle="light-content" />
             <View style={styles.container}>
                 <View style={styles.topContent}>
                     <Text style={styles.mainText}>Social Auth</Text>
+                    {authorizedUser === true ? (
+                        <Text style={{ color: '#FFFFFF' }}>
+                            welcome {user?.user.name}
+                        </Text>
+                    ) : (
+                        <></>
+                    )}
                 </View>
                 <View style={styles.bottomContent}>
-                    <TouchableOpacity
-                        onPress={signinWithGoogle}
-                        style={styles.googleButton}
-                    >
-                        <Image
-                            style={styles.googleIcon}
-                            source={{
-                                uri: 'https://i.ibb.co/j82DCcR/search.png'
-                            }}
-                        />
-                        <Text style={styles.googleButtonText}>
-                            Sign in with Google
-                        </Text>
-                    </TouchableOpacity>
+                    {authorizedUser ? (
+                        <TouchableOpacity
+                            onPress={logoutUser}
+                            style={styles.googleButton}
+                        >
+                            <Image
+                                style={styles.googleIcon}
+                                source={{
+                                    uri: 'https://i.ibb.co/j82DCcR/search.png'
+                                }}
+                            />
+                            <Text style={styles.googleButtonText}>Log out</Text>
+                        </TouchableOpacity>
+                    ) : (
+                        <TouchableOpacity
+                            onPress={signInWithGoogle}
+                            style={styles.googleButton}
+                        >
+                            <Image
+                                style={styles.googleIcon}
+                                source={{
+                                    uri: 'https://i.ibb.co/j82DCcR/search.png'
+                                }}
+                            />
+                            <Text style={styles.googleButtonText}>
+                                Sign in with Google
+                            </Text>
+                        </TouchableOpacity>
+                    )}
                 </View>
             </View>
         </SafeAreaView>
---
+++ frontend/Interfaces/I_GoogleUser.tsx
@@ -0,0 +1,13 @@
+export interface I_GoogleUser {
+    displayName: string | null;
+    email: string | null;
+    emailVerified: boolean;
+    isAnonymous: boolean;
+    metadata: {};
+    multiFactor: {} | null;
+    phoneNumber: string | null;
+    photoURL: string | null;
+    providerData: any[];
+    providerId: string;
+    uid: string;
+}
