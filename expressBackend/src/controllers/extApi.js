ab43e2dbed67769c6ba0f085945ead0c456716ad
teventov5 <teventov@gmail.com>
2023-04-23 18:21:35 +0300

    fixed headshot api again

--- expressBackend/src/controllers/extApi.js
+++ expressBackend/src/controllers/extApi.js
@@ -33,12 +33,12 @@ function parsePlayerInfo(playerInfo) {
   } = playerInfo;
 
   // console.log("name to look is:");
-  keyInDict = `${first_name} ${last_name}`.toLowerCase();
+  keyInJson = `${first_name} ${last_name}`.toLowerCase();
 
   // console.log(keyInDict);
   // console.log("dict is:");
   // console.log(playersHeadshotDictionary[keyInDict]);
-  let headshot_id = playersHeadshotDictionary[keyInDict];
+  let headshot_id = playersHeadshotDictionary[keyInJson];
   const img_uri = `https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${headshot_id}.png`;
 
   const parsedPlayerInfo = {
